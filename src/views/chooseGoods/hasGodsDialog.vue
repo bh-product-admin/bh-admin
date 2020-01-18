350d2e71aa9073986506747112c9265bc78bd8fd
yuqiqi <yuqiqi@ewell.cc>
2020-01-18 12:11:32 +0800

    我有货

--- src/views/chooseGoods/hasGodsDialog.vue
+++ src/views/chooseGoods/hasGodsDialog.vue
@@ -1,79 +1,109 @@
 <template>
   <div class="index">
     <el-dialog title="我有货" :visible.sync="dialogFormVisible">
-      <el-form :model="form" label-width="120px">
+      <el-form ref="ruleForm" :model="form" label-width="120px" :rules="rules">
         <el-form-item label="">
-          <el-radio-group v-model="form.resource" class="sku-box">
-            <el-radio label="我有相同sku" />
-            <el-radio label="我有相似相同sku" />
+          <el-radio-group v-model="form.skuType" class="sku-box">
+            <el-radio label="1">我有相同sku</el-radio>
+            <el-radio label="2">我有相似相同sku</el-radio>
           </el-radio-group>
         </el-form-item>
-        <el-form-item label="商品标题：">
-          <span>{{ form.title }}</span>
+        <el-form-item label="商品标题：" prop="title">
+          <el-input v-model="form.title" placeholder="商品标题" />
         </el-form-item>
         <el-form-item label="商品图：">
-          <img :src="form.goodsImg" width="100" height="100" alt="">
+          <img :src="form.img" width="100" height="100" alt="">
         </el-form-item>
-        <el-form-item label="采购单价：">
+        <el-form-item label="采购单价：" prop="price">
           <el-input v-model="form.price" placeholder="元/件" />
         </el-form-item>
         <el-row>
           <el-col :span="12">
-            <el-form-item label="库存量：">
-              <el-input v-model="form.kucun" />
+            <el-form-item label="库存量：" prop="inventory">
+              <el-input v-model="form.inventory" />
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="日生产力：">
-              <el-input v-model="form.shengchan" placeholder="日/天" />
+            <el-form-item label="日生产力：" prop="dayProductionNum">
+              <el-input v-model="form.dayProductionNum" placeholder="日/天" />
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="12">
-            <el-form-item label="日发货：">
-              <el-input v-model="form.fahuo" placeholder="件/天" />
+            <el-form-item label="日发货：" prop="daySendNum">
+              <el-input v-model="form.daySendNum" placeholder="件/天" />
             </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="退货比例：">
-              <el-input v-model="form.bili" placeholder="0-100%" />
+            <el-form-item label="退货比例：" prop="returnNum">
+              <el-input v-model="form.returnNum" placeholder="0-100%" />
             </el-form-item>
           </el-col>
         </el-row>
-        <el-form-item label="最长销售周期：">
-          <el-input v-model="form.zhouqi" placeholder="" />
+        <el-form-item label="最长销售周期：" prop="maxSaleCycle">
+          <el-input v-model="form.maxSaleCycle" placeholder="" />
         </el-form-item>
         <el-form-item label="备注：">
-          <el-input v-model="form.remark" type="textarea" />
+          <el-input v-model="form.note" type="textarea" />
           <p class="cred p0">您还不是认证厂家，点击前往认证，买家购买更放心！</p>
         </el-form-item>
 
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+        <el-button type="primary" @click="onSubmit">确 定</el-button>
       </div>
     </el-dialog>
   </div>
 </template>
 <script>
+import {
+  hasGoodAdd
+} from '@/api/chooseGoods'
 export default {
   name: 'HasGoodsDialog',
   props: [],
   data() {
     return {
       form: {
-        resource: '',
-        title: 'titletitletitle',
-        goodsImg: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577718746219&di=86de817649061d34f4fe193d290e1c11&imgtype=0&src=http%3A%2F%2Fa3.att.hudong.com%2F46%2F79%2F01300000921826131812790368314.jpg',
+        skuType: '1',
+        title: '',
+        img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577718746219&di=86de817649061d34f4fe193d290e1c11&imgtype=0&src=http%3A%2F%2Fa3.att.hudong.com%2F46%2F79%2F01300000921826131812790368314.jpg',
         price: '',
-        kucun: '',
-        shengchan: '',
-        rifahuo: '',
-        bili: '',
-        zhouqi: '',
-        remark: ''
+        inventory: '',
+        dayProductionNum: '',
+        daySendNum: '',
+        returnNum: '',
+        maxSaleCycle: '',
+        note: '',
+        id: ''
+      },
+      rules: {
+        title: [
+          { required: true, message: '请输入商品标题', trigger: 'blur' }
+        ],
+        price: [
+          { required: true, message: '请输入采购单价', trigger: 'blur' }
+        ],
+        inventory: [
+          { required: true, message: '请输入库存量', trigger: 'blur' }
+        ],
+        dayProductionNum: [
+          { required: true, message: '请输入日发货', trigger: 'blur' }
+        ],
+        daySendNum: [
+          { required: true, message: '请输入日发货', trigger: 'blur' }
+        ],
+        returnNum: [
+          { required: true, message: '请输入日发货', trigger: 'blur' }
+        ],
+        img: [
+          { required: true, message: '请上传头像', trigger: 'blur' }
+        ],
+        maxSaleCycle: [
+          { required: true, message: '请输入日发货', trigger: 'blur' }
+        ]
       },
       formLabelWidth: '120px',
       dialogFormVisible: false
@@ -82,10 +112,27 @@ export default {
   created() {},
   methods: {
     onSubmit() {
-      alert('submit!')
+      this.$refs['ruleForm'].validate((valid) => {
+        if (valid) {
+          console.log('验证通过！')
+          this.dialogFormVisible = false
+          const params = this.form
+          console.log(params)
+          hasGoodAdd(this.form).then(res => {
+            console.log(res, 'resdsdsd-has')
+          }).catch(err => {
+            console.log(err, 'errhas')
+          })
+        } else {
+          this.$message.error('请检查页面必填项')
+          return false
+        }
+      })
     },
     showDialog(item, isDialogFormVisible) {
+      console.log(item, 'indedsds')
       this.dialogFormVisible = isDialogFormVisible
+      this.form.id = item && item.id
     }
   }
 }
